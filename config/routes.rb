5627d05543dfca7a3b696f5f0841a885d34faa21
Silvain <silvain@agencyleroy.com>
2015-10-13 11:39:43 +0300

    gets label for a single shipment at a time (enables address updates)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Spree::Core::Engine.add_routes do
-  get 'admin/orders/:id/matkahuolto_labels' => 'admin/orders#matkahuolto_labels', as: :get_matkahuolto_labels_admin_order
+  get 'admin/orders/:id/matkahuolto_labels/:shipment_id' => 'admin/orders#matkahuolto_labels', as: :get_matkahuolto_labels_admin_order
 end
--- lib/controllers/spree/admin/orders_controller_decorator.rb
+++ lib/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,8 +1,9 @@
 Spree::Admin::OrdersController.class_eval do
 
   def matkahuolto_labels
-    @order = Spree::Order.find_by(number:params[:id])
-    return unless @order
+
+    s = Spree::Shipment.find_by(number:params[:shipment_id])
+    return unless s
 
     label_api = SpreeMatkahuolto::API::ShippingLabel.new(
       Rails.configuration.x.matkahuolto_username,
@@ -15,58 +16,54 @@ Spree::Admin::OrdersController.class_eval do
 
     shipments = []
 
-    @order.shipments.each do |s|
-      # TODO: Change API Login
-      m_shipment = SpreeMatkahuolto::Shipment.new(Rails.configuration.x.matkahuolto_username)
+    # TODO: Change API Login
+    m_shipment = SpreeMatkahuolto::Shipment.new(Rails.configuration.x.matkahuolto_username)
 
-      if s.shipping_method.admin_name.include? "matkahuolto_lahella"
-        destination_code = Spree::MatkahuoltoShipment.where(order_id: @order.id).first.try(:destination_code)
+    if s.shipping_method.admin_name.include? "matkahuolto_lahella"
+      destination_code = Spree::MatkahuoltoShipment.where(order_id: @order.id).first.try(:destination_code)
 
-        if destination_code
-          m_shipment.destination_place_code = destination_code
-          m_shipment.product_code = "80"
-        end
+      if destination_code
+        m_shipment.destination_place_code = destination_code
+        m_shipment.product_code = "80"
       end
-
-      m_shipment.sender_reference = "Order: #{@order.number}"
-      m_shipment.receiver_name = "#{s.address.first_name} #{s.address.last_name}"
-      m_shipment.receiver_address = "#{s.address.address1} #{s.address.address2}"
-      m_shipment.receiver_postal = "#{s.address.zipcode}"
-      m_shipment.receiver_city = "#{s.address.city}"
-      m_shipment.receiver_contact_name = "#{s.address.first_name} #{s.address.last_name}"
-      m_shipment.receiver_contact_number = "#{s.address.phone}"
-      m_shipment.receiver_email = "#{@order.email}"
-      total_weight = 0.0
-      s.line_items.each do |item|
-        total_weight += item.try(:product).try(:weight)
-      end
-      if total_weight == 0.0
-        total_weight = 1.0
-      end
-      m_shipment.weight = total_weight.to_s
-
-      
-      custom_s = SpreeMatkahuoltoCustomShipment.find_by(shipment_id:s.id)
-      if custom_s and custom_s.label_file_name
-        # m_shipment.message_type = "C"
-        return send_file custom_s.label.path, type: "application/pdf"
-      end
-
-      shipments.push m_shipment
     end
+
+    m_shipment.sender_reference = "Order: #{@order.number}"
+    m_shipment.receiver_name = "#{s.address.first_name} #{s.address.last_name}"
+    m_shipment.receiver_address = "#{s.address.address1} #{s.address.address2}"
+    m_shipment.receiver_postal = "#{s.address.zipcode}"
+    m_shipment.receiver_city = "#{s.address.city}"
+    m_shipment.receiver_contact_name = "#{s.address.first_name} #{s.address.last_name}"
+    m_shipment.receiver_contact_number = "#{s.address.phone}"
+    m_shipment.receiver_email = "#{@order.email}"
+    total_weight = 0.0
+    s.line_items.each do |item|
+      total_weight += item.try(:product).try(:weight)
+    end
+    if total_weight == 0.0
+      total_weight = 1.0
+    end
+    m_shipment.weight = total_weight.to_s
+
+    
+    custom_s = SpreeMatkahuoltoCustomShipment.find_by(shipment_id:s.id)
+    if custom_s and custom_s.label_file_name
+      # m_shipment.message_type = "C"
+      return send_file custom_s.label.path, type: "application/pdf"
+    end
+
+    shipments.push m_shipment
     
     label = label_api.get_labels(shipments)
 
-    @order.shipments.each do |s|
-      custom_s = SpreeMatkahuoltoCustomShipment.find_by(shipment_id:s.id)
-      if not custom_s
-        custom_s = SpreeMatkahuoltoCustomShipment.create(shipment_id:s.id)
-      end
-      label_path = label[:path]
-      label_file = File.open(label_path, 'r')
-      custom_s.label = label_file
-      custom_s.save
+    custom_s = SpreeMatkahuoltoCustomShipment.find_by(shipment_id:s.id)
+    if not custom_s
+      custom_s = SpreeMatkahuoltoCustomShipment.create(shipment_id:s.id)
     end
+    label_path = label[:path]
+    label_file = File.open(label_path, 'r')
+    custom_s.label = label_file
+    custom_s.save
 
     send_file label[:path], filename: label[:filename], type: "application/pdf"
   end
